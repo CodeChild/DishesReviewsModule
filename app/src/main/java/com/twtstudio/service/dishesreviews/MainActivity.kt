78f7865873c2678fe6988da94d4782e6ef1998ef
CodeChild <zhangyulong1997@hotmail.com>
2018-03-16 19:11:03 +0800

    [add] 添加LazyFragment以及主界面点击bottom bar切换页面

--- app/src/main/java/com/twtstudio/service/dishesreviews/MainActivity.kt
+++ app/src/main/java/com/twtstudio/service/dishesreviews/MainActivity.kt
@@ -12,29 +12,34 @@ import android.view.View
  * Created by zhangyulong on 18-3-11.
  */
 class MainActivity : AppCompatActivity() {
-    lateinit var bottomNavigationView: BottomNavigationView
-    lateinit var viewPager: ViewPager
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var viewPager: ViewPager
+    private lateinit var viewPagerAdapter: MainPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dishes_reviews_activity_main)
         bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         viewPager=findViewById<ViewPager>(R.id.vp_main)
+        viewPagerAdapter= MainPagerAdapter(supportFragmentManager)
+        viewPager.adapter=viewPagerAdapter
+
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 object : BottomNavigationView.OnNavigationItemSelectedListener {
                     override fun onNavigationItemSelected(item: MenuItem): Boolean {
                         when (item.getItemId()) {
                             R.id.action_home -> {
-
+                                viewPager.currentItem= item.order
                             }
                             R.id.action_fast_comment -> {
 
                             }
                             R.id.action_account -> {
-
+                                viewPager.currentItem= item.order
                             }
                         }
                         return true
                     }
                 })
+
     }
 }
---
+++ app/src/main/java/com/twtstudio/service/dishesreviews/home/view/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.twtstudio.service.dishesreviews.home.view
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.twtstudio.service.dishesreviews.R
+import com.twtstudio.service.dishesreviews.base.LazyFragment
+
+
+class HomeFragment : LazyFragment() {
+    override fun getResId(): Int {
+        return R.layout.dishes_reviews_fragment_home
+    }
+
+    override fun onRealViewLoaded(view: View) {
+
+    }
+
+
+}
