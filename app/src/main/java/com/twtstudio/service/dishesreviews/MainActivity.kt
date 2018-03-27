63f99b01b38545c93eecc6986cd861b3f9d4f409
CodeChild <zhangyulong1997@hotmail.com>
2018-03-27 23:04:51 +0800

    [mod] 在主页添加toolbar

--- app/src/main/java/com/twtstudio/service/dishesreviews/MainActivity.kt
+++ app/src/main/java/com/twtstudio/service/dishesreviews/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.twtstudio.service.dishesreviews
 
 import android.content.res.ColorStateList
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import com.twtstudio.service.dishesreviews.base.CustomViewPager
 
 
@@ -17,6 +22,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var viewPager: CustomViewPager
     private lateinit var viewPagerAdapter: MainPagerAdapter
+    private lateinit var toolbar: Toolbar
+    private lateinit var tvTitle: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dishes_reviews_activity_main)
@@ -27,6 +34,17 @@ class MainActivity : AppCompatActivity() {
             adapter = viewPagerAdapter
             setPagingEnabled(false)
         }
+        toolbar = findViewById<Toolbar>(R.id.toolbar).apply {
+            title = ""
+        }
+        setSupportActionBar(toolbar)
+        getSupportActionBar()?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.dishes_reviews_ic_action_back)
+        }
+        tvTitle = findViewById<TextView>(R.id.tv_toolbar_title).apply {
+            text = getString(R.string.app_name)
+        }
 
         bottomNavigationView.apply {
             setOnNavigationItemSelectedListener(
@@ -35,20 +53,42 @@ class MainActivity : AppCompatActivity() {
                             when (item.getItemId()) {
                                 R.id.action_home -> {
                                     viewPager.currentItem = item.order
+                                    tvTitle.text = getString(R.string.app_name)
                                 }
                                 R.id.action_fast_comment -> {
-
+                                    menu?.findItem(R.id.menu_search)?.setVisible(false)
                                 }
                                 R.id.action_account -> {
                                     viewPager.currentItem = item.order
+                                    tvTitle.text = getString(R.string.text_account)
+                                    menu?.findItem(R.id.menu_search)?.setVisible(false)
                                 }
                             }
+                            invalidateOptionsMenu()
                             return true
                         }
                     })
-            itemIconTintList= null //不设置IconTint
+            itemIconTintList = null //不设置IconTint
         }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.dishes_reviews_menu_home_toolbar, menu)
+        return true
+    }
 
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        when (viewPager.currentItem) {
+            0 -> menu?.findItem(R.id.menu_search)?.setVisible(true)
+            else -> menu?.findItem(R.id.menu_search)?.setVisible(false)
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/twtstudio/service/dishesreviews/home/view/HomeFragment.kt
+++ app/src/main/java/com/twtstudio/service/dishesreviews/home/view/HomeFragment.kt
@@ -1,12 +1,16 @@
 package com.twtstudio.service.dishesreviews.home.view
 
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.twtstudio.service.dishesreviews.R
 import com.twtstudio.service.dishesreviews.base.LazyFragment
 
@@ -24,6 +28,7 @@ class HomeFragment : LazyFragment() {
                     HomePagerAdapter.AD,
                     HomePagerAdapter.REVIEWS),context,this@HomeFragment)
         }
+
     }
 
 
